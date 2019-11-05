4f2a048d96900d10ad840b501cdd41f9d2255f6d
theChildinus <zky0825@163.com>
2019-11-05 15:38:32 +0800

    add check func

--- src/main/java/Check.java
+++ src/main/java/Check.java
@@ -1,2 +1,44 @@
+import org.json.simple.JSONObject;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+
 public class Check {
+    // 开发环境url
+    private static final String devUrlPrefix = "http://localhost:9090/";
+    // 正式环境url
+    private static final String proUrlPrefix = "https://10.108.165.181:4433/api/";
+
+    // 每次登录后randNum值均不一样
+    public static String Check(String username, String randNum, String sub, String obj, String act) throws Exception{
+        String url = proUrlPrefix;
+        String resp;
+        JSONObject jsonWrite = new JSONObject();
+        System.out.println("====== 策略验证过程 ======");
+        String str = username + randNum;
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        // 计算md5函数
+        md.update(str.getBytes());
+        String md5sum = new BigInteger(1, md.digest()).toString(16);
+        System.out.println(md5sum);
+        //Inserting key-value pairs into the json object
+        jsonWrite.put("username", username);
+        jsonWrite.put("userhash", md5sum);
+        jsonWrite.put("policysub", sub);
+        jsonWrite.put("policyobj", obj);
+        jsonWrite.put("policyact", act);
+        System.out.println("CHECK: " + jsonWrite.toJSONString());
+        if (url.contains("https")) {
+            resp = HttpsPost.doPost(proUrlPrefix + "policy/check", jsonWrite.toJSONString());
+        } else {
+            resp = HttpPost.doPost(devUrlPrefix + "policy/check", jsonWrite.toJSONString());
+        }
+        System.out.println("RESP: " + resp);
+        return resp;
+    }
+
+    public static void main(String[] args) throws Exception{
+        String randNum = "8121576815539813105";
+        Check("zhao", randNum,"zhao", "data4", "exec");
+    }
 }
--- src/main/java/Login.java
+++ src/main/java/Login.java
@@ -9,31 +9,32 @@ public class Login {
     // 开发环境url
     private static final String devUrlPrefix = "http://localhost:9090/";
     // 正式环境url
-    private static final String proUrlPrefix = "https://localhost/api/";
+    private static final String proUrlPrefix = "https://10.108.165.181:4433/api/";
     private static final String opensslPath = "C:\\Program Files\\Git\\usr\\bin\\openssl";
-    private static final String privateKeyPath = "./pairs/zhao.pem";
 
-    public static void main(String[] args) throws Exception{
+    public static String Login(String username, String privateKeyPath) throws Exception {
         String url = proUrlPrefix;
         String resp1;
         String resp2;
+        System.out.println("====== 登录过程 ======");
         JSONObject jsonWrite = new JSONObject();
         //Inserting key-value pairs into the json object
-        jsonWrite.put("username", "zhao");
+        jsonWrite.put("username", username);
         System.out.println("STEP 1: " + jsonWrite.toJSONString());
         if (url.contains("https")) {
             resp1 = HttpsPost.doPost(proUrlPrefix + "user/login", jsonWrite.toJSONString());
         } else {
             resp1 = HttpPost.doPost(devUrlPrefix + "user/login", jsonWrite.toJSONString());
         }
-        System.out.println(resp1);
+        System.out.println("RESP 1: " + resp1);
 
         JSONObject jsonRead = (JSONObject) new JSONParser().parse(resp1);
         Long code = (Long)jsonRead.get("code");
         System.out.println(code);
+
         PrivateKey privateKey = SignWithEC.getPrivateKey(privateKeyPath, opensslPath);
         byte[] s = SignWithEC.sign(privateKey, String.valueOf(code));
-        System.out.println("签名："+ HexBin.encode(s));
+        System.out.println("签名："+ Base64.encode(s));
 
         jsonWrite.put("userrand", code);
         jsonWrite.put("usersign", Base64.encode(s));
@@ -43,6 +44,11 @@ public class Login {
         } else {
             resp2 = HttpPost.doPost(devUrlPrefix + "user/login", jsonWrite.toJSONString());
         }
-        System.out.println(resp2);
+        System.out.println("RESP 2: " + resp2);
+        return resp2;
+    }
+
+    public static void main(String[] args) throws Exception{
+        Login("zhao", "./pairs/zhao.pem");
     }
 }
