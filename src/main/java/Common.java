e552db37572f24f59f450b843b936ffed3b29173
theChildinus <zky0825@163.com>
2019-11-18 10:03:34 +0800

    add verify cert example

--- src/main/java/Common.java
+++ src/main/java/Common.java
@@ -1,7 +1,24 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Common {
     // 开发环境url
     public static final String devUrlPrefix = "http://localhost:9090/";
     // 正式环境url
     public static final String proUrlPrefix = "https://10.108.165.181:4433/api/";
     public static final String opensslPath = "C:\\Program Files\\Git\\usr\\bin\\openssl";
+
+    public static String getFileContent(FileInputStream fis, String encoding) throws IOException {
+        try(BufferedReader br = new BufferedReader( new InputStreamReader(fis, encoding))) {
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while(( line = br.readLine()) != null ) {
+                sb.append(line);
+                sb.append('\n');
+            }
+            return sb.toString();
+        }
+    }
 }
---
+++ src/main/java/VerifyCert.java
@@ -0,0 +1,30 @@
+import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
+import org.json.simple.JSONObject;
+import java.io.FileInputStream;
+
+public class VerifyCert {
+    public static String VerifyCert(String username, String certPath) throws Exception {
+        String url = Common.proUrlPrefix;
+        String resp;
+        JSONObject jsonWrite = new JSONObject();
+        System.out.println("====== 证书验证过程 ======");
+        FileInputStream fis = new FileInputStream(certPath);
+        String cert = Common.getFileContent(fis, "UTF-8");
+        //Inserting key-value pairs into the json object
+        jsonWrite.put("username", username);
+        jsonWrite.put("certcontent", Base64.encode(cert.getBytes()));
+        System.out.println("CHECK: " + jsonWrite.toJSONString());
+        if (url.contains("https")) {
+            resp = HttpsPost.doPost(Common.proUrlPrefix + "user/verifyCert", jsonWrite.toJSONString());
+        } else {
+            resp = HttpPost.doPost(Common.devUrlPrefix + "user/verifyCert", jsonWrite.toJSONString());
+        }
+        System.out.println("RESP: " + resp);
+        return resp;
+    }
+
+    public static void main(String[] args) throws Exception{
+        String username = "kong";
+        VerifyCert(username, "./pairs/" + username + ".crt");
+    }
+}
